0cb95a65aad46a0db62bf4a080c90166ac91d656
Bob Watson <rbwatson@uw.edu>
2014-04-02 21:31:35 -0700

    Updated paths & map image for new repo

--- extras/test/account_test.bat
+++ extras/test/account_test.bat
@@ -5,27 +5,27 @@ if "%hostpath%"=="" set hostpath=localhost
 if exist account_test_results.txt (del account_test_results.txt)
 echo {^"accountResults^":[ >  account_test_results.txt
 REM Error test
-curl -g http://%hostpath%/wlux/data/account.php?bad[some]=stuff >> account_test_results.txt
+curl -g http://%hostpath%/wluxui/data/account.php?bad[some]=stuff >> account_test_results.txt
 echo , >> account_test_results.txt
 REM GET tests
-curl --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wlux/data/account.php?user[username]=defaultResearcher >> account_test_results.txt
+curl --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wluxui/data/account.php?user[username]=defaultResearcher >> account_test_results.txt
 echo , >> account_test_results.txt
-curl --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wlux/data/account.php?user[username]=Nobody >> account_test_results.txt
+curl --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wluxui/data/account.php?user[username]=Nobody >> account_test_results.txt
 echo , >> account_test_results.txt
 REM POST test
-curl -X POST --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -d "bad[some]=stuff" -g http://%hostpath%/wlux/data/account.php >> account_test_results.txt
+curl -X POST --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -d "bad[some]=stuff" -g http://%hostpath%/wluxui/data/account.php >> account_test_results.txt
 echo , >> account_test_results.txt
-curl -X POST --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -d "user[some]=stuff" -g http://%hostpath%/wlux/data/account.php >> account_test_results.txt
+curl -X POST --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -d "user[some]=stuff" -g http://%hostpath%/wluxui/data/account.php >> account_test_results.txt
 echo , >> account_test_results.txt
-curl -X POST --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -d "changePassword[some]=stuff" -g http://%hostpath%/wlux/data/account.php >> account_test_results.txt
+curl -X POST --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -d "changePassword[some]=stuff" -g http://%hostpath%/wluxui/data/account.php >> account_test_results.txt
 echo , >> account_test_results.txt
 REM PUT test
-curl -X PUT --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wlux/data/account.php?bad[some]=stuff >> account_test_results.txt
+curl -X PUT --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wluxui/data/account.php?bad[some]=stuff >> account_test_results.txt
 echo , >> account_test_results.txt
-curl -X PUT --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wlux/data/account.php?user[username]=Nobody >> account_test_results.txt
+curl -X PUT --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wluxui/data/account.php?user[username]=Nobody >> account_test_results.txt
 echo , >> account_test_results.txt
 REM DELETE test
-curl -X DELETE --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wlux/data/account.php?bad[some]=stuff >> account_test_results.txt
+curl -X DELETE --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wluxui/data/account.php?bad[some]=stuff >> account_test_results.txt
 echo , >> account_test_results.txt
-curl -X DELETE --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wlux/data/account.php?user[username]=Nobody >> account_test_results.txt
+curl -X DELETE --header "Content-Length:0" --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wluxui/data/account.php?user[username]=Nobody >> account_test_results.txt
 echo ]} >> account_test_results.txt
--- extras/test/session_test.bat
+++ extras/test/session_test.bat
@@ -8,21 +8,21 @@ REM	test error case
 REM
 echo {^"sessionResults^":[ >  session_test_results.txt
 REM This just returns an error
-curl --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wlux/data/session.php?bad[some]=stuff >> session_test_results.txt
+curl --header "Authorization: Basic ZGVmYXVsdFJlc2VhcmNoZXI6MVBhc3N3b3Jk" -g http://%hostpath%/wluxui/data/session.php?bad[some]=stuff >> session_test_results.txt
 echo , >> session_test_results.txt
-curl -g http://%hostpath%/wlux/data/session.php?config[some]=stuff >> session_test_results.txt
+curl -g http://%hostpath%/wluxui/data/session.php?config[some]=stuff >> session_test_results.txt
 echo , >> session_test_results.txt
-curl -g http://%hostpath%/wlux/data/session.php?currentStep[some]=stuff >> session_test_results.txt
+curl -g http://%hostpath%/wluxui/data/session.php?currentStep[some]=stuff >> session_test_results.txt
 echo , >> session_test_results.txt
 REM These don't need an authorization header
-curl -X POST -d "bad[some]=stuff" -g http://%hostpath%/wlux/data/session.php >> session_test_results.txt
+curl -X POST -d "bad[some]=stuff" -g http://%hostpath%/wluxui/data/session.php >> session_test_results.txt
 echo , >> session_test_results.txt
-curl -X POST -d "start[some]=stuff" -g http://%hostpath%/wlux/data/session.php >> session_test_results.txt
+curl -X POST -d "start[some]=stuff" -g http://%hostpath%/wluxui/data/session.php >> session_test_results.txt
 echo , >> session_test_results.txt
-curl -X POST -d "startNextStepsome]=stuff" -g http://%hostpath%/wlux/data/session.php >> session_test_results.txt
+curl -X POST -d "startNextStepsome]=stuff" -g http://%hostpath%/wluxui/data/session.php >> session_test_results.txt
 echo , >> session_test_results.txt
-curl -X POST -d "finishCurrentStep[some]=stuff" -g http://%hostpath%/wlux/data/session.php >> session_test_results.txt
+curl -X POST -d "finishCurrentStep[some]=stuff" -g http://%hostpath%/wluxui/data/session.php >> session_test_results.txt
 echo , >> session_test_results.txt
 REM This should return a 405 error
-curl -X PATCH --header "Content-Length: 0" -g http://%hostpath%/wlux/data/session.php?start[some]=stuff >> session_test_results.txt
+curl -X PATCH --header "Content-Length: 0" -g http://%hostpath%/wluxui/data/session.php?start[some]=stuff >> session_test_results.txt
 echo ]} >> session_test_results.txt
